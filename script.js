808dfe5d78d570578cd4c60c93d93a2bbb794f04
doniwirawan <doniwirawan166@gmail.com>
2020-11-29 11:48:43 +0800

    adding changeBG feature and Quote

--- script.js
+++ script.js
@@ -28,6 +28,63 @@ function updateCountdown(){
 	seconds.innerHTML= s < 10 ? '0' + s : s;
 }
 
+const changeBg = () => {
+    fetch('https://api.unsplash.com/photos/random?client_id=gfXzHUZJnwnsObUFyB0hxiOMXrt_nA4V7UyjpCrRK6w')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.urls.regular);
+            const bgEl = document.querySelector('body');
+            bgEl.style.backgroundImage = `url('${data.urls.regular}')`;
+        })
+}
+
+fetch("https://type.fit/api/quotes")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // menambahkan quote secara default
+        const root = document.querySelector('.quote-area');
+        const randoms = Math.floor(Math.random() * data.length);
+        root.innerHTML = `
+                    <p>"${data[randoms].text}"</p>
+                    <p>-- ${data[randoms].author} --</p>
+                `
+
+        // ketika diklik maka akan generate quote baru
+        document.querySelector('.button').addEventListener('click', function () {
+            const random = Math.floor(Math.random() * data.length)
+            if (data[random].author === null) {
+                root.innerHTML = `
+                    <p>"${data[random].text}"</p>
+                    <p>-- ${data[random].author = 'Unknown'} --</p>
+                `
+            } else {
+                root.innerHTML = `
+                    <p>"${data[random].text}"</p>
+                    <p>-- ${data[random].author} --</p>
+                `
+
+            }
+
+
+
+        })
+
+
+    });
+
+
+document.querySelector('.button2').addEventListener('click', changeBg);
+
+
+
+
+
+
+
+
+
 setTimeout(()=>{
 	loading.remove();
 	countdown.style.display= 'flex';
