e81c2d87ed7f24e802523c50aeb38266e0623a17
doniwirawan <doniwirawan166@gmail.com>
2021-01-02 22:23:57 +0800

    css for mobile is done

--- script.js
+++ script.js
@@ -5,27 +5,29 @@ const seconds = document.querySelector('#seconds');
 const countdown = document.querySelector('#countdown');
 const year = document.querySelector('#year');
 const loading = document.querySelector('#loading');
+const h1 = document.querySelector('#h1');
+const btn_container = document.querySelector('#btn-container');
 
 
 const currentYear = new Date().getFullYear();
 
-const newYearTime = new Date(`January 01 ${currentYear +1} 00:00:00:00`);
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00:00`);
 
 //set background year
 year.innerText = currentYear + 1;
 
-function updateCountdown(){
-	const currentTime = new Date();
-	const diff = newYearTime - currentTime;
-	const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-	const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-	const m = Math.floor(diff / 1000 / 60) % 60;
-	const s = Math.floor(diff / 1000) % 60;
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
 
-	days.innerHTML= d;
-	hours.innerHTML= h < 10 ? '0' + h : h;
-	minutes.innerHTML= m < 10 ? '0' + m : m;
-	seconds.innerHTML= s < 10 ? '0' + s : s;
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
 const changeBg = () => {
@@ -85,8 +87,11 @@ document.querySelector('.button2').addEventListener('click', changeBg);
 
 
 
-setTimeout(()=>{
-	loading.remove();
-	countdown.style.display= 'flex';
-},1000)
-setInterval(updateCountdown,1000);
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+    h1.style.display = 'flex';
+    btn_container.style.display = 'flex';
+    year.style.display = 'flex';
+}, 1000)
+setInterval(updateCountdown, 1000);
